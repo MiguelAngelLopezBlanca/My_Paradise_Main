f61201eadfb523cc6c7555b46630d81c9c0e3a70
rhgarcia88 <rhgarguer@gmail.com>
2021-05-29 11:39:33 +0200

    Datos Mas

--- lib/bottomNavigationItem/mas.dart
+++ lib/bottomNavigationItem/mas.dart
@@ -46,42 +46,41 @@ class _Mas extends State<Mas> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
+        child: ListView.builder(
+          itemCount: _modelos.length,
           scrollDirection: Axis.horizontal,
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 10, 20, 10),
-              child: Container(
-                width: returnResponsiveWidth(context, 0.95),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/house.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30),
-                    ),
-                    color: Colors.black.withAlpha(120),
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Modelo 1',
-                    style: TextStyle(
-                        color: colorBotones,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600),
-                  ),
+          itemBuilder: (context, index) {
+            return Container(
+              width: returnResponsiveWidth(context, 0.95),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: AssetImage(
+                      "assets/images/modelos/" + _modelos[index].nombreImagen),
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ],
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                  color: Colors.black.withAlpha(120),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'Modelo 1',
+                  style: TextStyle(
+                      color: colorBotones,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
