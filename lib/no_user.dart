0238467f9ce6c4cd09064a7cbb798424f7192637
Miguel Angel <51151697+MiguelAngelLopezBlanca@users.noreply.github.com>
2021-06-02 19:52:56 +0200

    commit pantalla noUser.dart

---
+++ lib/no_user.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:my_paradise/ui_constants.dart';
+
+class noUser extends StatefulWidget {
+  _noUser createState() => _noUser();
+}
+
+class _noUser extends State<noUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SafeArea(
+              child: FractionallySizedBox(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.width / 1,
+                          top: MediaQuery.of(context).size.height / 20),
+                    ),
+                    Text(
+                      'No has iniciado sesión, debes iniciar sesion para las siguientes tareas:',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    OutlinedButton(
+                      onPressed: () async {},
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.all(10.0),
+                        backgroundColor: colorBotones,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(40),
+                            right: Radius.circular(
+                              40,
+                            ),
+                          ),
+                        ),
+                      ),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 1.3,
+                        child: Text(
+                          "Iniciar Sesión",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
