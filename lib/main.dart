715e26dbf47cf8ef9ab565037abc757e94b63227
Miguel Angel <51151697+MiguelAngelLopezBlanca@users.noreply.github.com>
2021-06-04 20:24:00 +0200

    commit main navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:my_paradise/bottomNavigationItem/mas.dart';
 
+import 'bottomNavigation/menu.dart';
 import 'home_page.dart';
 
 void main() {
@@ -12,6 +16,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  Widget comprobarUsuario(BuildContext context) {
+    if (auth.currentUser != null) {
+      return Menu();
+    } else {
+      return HomePage();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -23,7 +37,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.blue),
-      home: HomePage(),
+      home: comprobarUsuario(context),
     );
   }
 }
