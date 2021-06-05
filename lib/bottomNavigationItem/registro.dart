f7804722d71f5181339907c2194d88d1152e0112
Miguel Angel <51151697+MiguelAngelLopezBlanca@users.noreply.github.com>
2021-06-05 18:08:44 +0200

    registro.dart

--- lib/bottomNavigationItem/registro.dart
+++ lib/bottomNavigationItem/registro.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:my_paradise/home_page.dart';
 
 import '../ui_constants.dart';
@@ -13,6 +11,9 @@ class Registro extends StatefulWidget {
 }
 
 class _Registro extends State<Registro> {
+  bool _isHidden = true;
+  bool _isHiddenPassConfirm = true;
+
   void handleNavigateTapToHome(BuildContext context) {
     Navigator.of(context).push(
       CupertinoPageRoute(
@@ -81,6 +82,18 @@ class _Registro extends State<Registro> {
     }
   }
 
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  void _togglePasswordConfirmView() {
+    setState(() {
+      _isHiddenPassConfirm = !_isHiddenPassConfirm;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,84 +101,142 @@ class _Registro extends State<Registro> {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(padding: const EdgeInsets.fromLTRB(0, 60, 0, 0)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        'Registrarme',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
-                            fontSize: returnResponsiveFontSize(context, 30)),
-                      ),
-                    ],
-                  ),
-                ],
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/fondo_registro.jpg'),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              FractionallySizedBox(
-                widthFactor: 0.75,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(padding: const EdgeInsets.fromLTRB(0, 60, 0, 0)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    TextField(
-                        decoration: new InputDecoration(
-                          fillColor: fondoTextField,
-                          filled: true,
-                          hintText: "Nombre",
-                          hintStyle: TextStyle(color: colorPrincipal),
-                          contentPadding:
-                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                          border: new OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                              const Radius.circular(10.0),
+                    Column(
+                      children: [
+                        Text(
+                          'Registrate',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                              fontSize: returnResponsiveFontSize(context, 30)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width / 1,
+                          child: Text(
+                            'Disfruta de todas las ventajas de tener una cuenta',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                                fontSize:
+                                    returnResponsiveFontSize(context, 20)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width / 1,
+                          height: MediaQuery.of(context).size.height / 4,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image:
+                                  AssetImage('assets/images/iconos/logo.png'),
                             ),
                           ),
                         ),
-                        onChanged: (value) {
-                          dsName = value;
-                        }),
-                    const SizedBox(
-                      height: 20,
+                      ],
                     ),
-                    TextField(
-                        decoration: new InputDecoration(
-                          fillColor: fondoTextField,
-                          filled: true,
-                          hintText: "Email",
-                          hintStyle: TextStyle(color: colorPrincipal),
-                          contentPadding:
-                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                          border: new OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                              const Radius.circular(10.0),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.75,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextField(
+                          decoration: new InputDecoration(
+                            fillColor: fondoTextField,
+                            filled: true,
+                            hintText: "Nombre",
+                            hintStyle: TextStyle(color: colorPrincipal),
+                            contentPadding:
+                                EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            border: new OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+                                const Radius.circular(10.0),
+                              ),
                             ),
                           ),
-                        ),
-                        onChanged: (value) {
-                          mail = value;
-                        }),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
+                          onChanged: (value) {
+                            dsName = value;
+                          }),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                          decoration: new InputDecoration(
+                            fillColor: fondoTextField,
+                            filled: true,
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: colorPrincipal),
+                            contentPadding:
+                                EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            border: new OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+                                const Radius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          onChanged: (value) {
+                            mail = value;
+                          }),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        obscureText: _isHidden,
                         decoration: new InputDecoration(
                           fillColor: fondoTextField,
                           filled: true,
                           hintText: "Contraseña",
                           hintStyle: TextStyle(color: colorPrincipal),
-                          suffixIcon: const Icon(
-                            Icons.remove_red_eye_rounded,
-                            color: colorPrincipal,
+                          suffixIcon: InkWell(
+                            onTap: _togglePasswordView,
+                            child: Icon(
+                              _isHidden
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
                           ),
                           contentPadding:
                               EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
@@ -177,58 +248,66 @@ class _Registro extends State<Registro> {
                         ),
                         onChanged: (value) {
                           passwd = value;
-                        }),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                        decoration: new InputDecoration(
-                          fillColor: fondoTextField,
-                          filled: true,
-                          hintText: "Confirmar Contraseña",
-                          hintStyle: TextStyle(color: colorPrincipal),
-                          suffixIcon: const Icon(
-                            Icons.remove_red_eye_rounded,
-                            color: colorPrincipal,
-                          ),
-                          contentPadding:
-                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                          border: new OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                              const Radius.circular(10.0),
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                          obscureText: _isHiddenPassConfirm,
+                          decoration: new InputDecoration(
+                            fillColor: fondoTextField,
+                            filled: true,
+                            hintText: "Confirmar Contraseña",
+                            hintStyle: TextStyle(color: colorPrincipal),
+                            suffixIcon: InkWell(
+                              onTap: _togglePasswordConfirmView,
+                              child: Icon(
+                                _isHiddenPassConfirm
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                            ),
+                            contentPadding:
+                                EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            border: new OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+                                const Radius.circular(10.0),
+                              ),
                             ),
                           ),
-                        ),
-                        onChanged: (value) {
-                          passwdV = value;
-                        }),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 0)),
-                    OutlinedButton(
-                      onPressed: () =>
-                          {registerWithEmailAndPassword(dsName, passwd, mail)},
-                      style: OutlinedButton.styleFrom(
-                        padding: const EdgeInsets.all(10.0),
-                        backgroundColor: colorBotones,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
+                          onChanged: (value) {
+                            passwdV = value;
+                          }),
+                      const SizedBox(
+                        height: 20,
                       ),
-                      child: const Text(
-                        "Registrarme",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
+                      Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 0)),
+                      OutlinedButton(
+                        onPressed: () => {
+                          registerWithEmailAndPassword(dsName, passwd, mail)
+                        },
+                        style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.all(10.0),
+                          backgroundColor: colorBotones,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                        ),
+                        child: const Text(
+                          "Registrarme",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
